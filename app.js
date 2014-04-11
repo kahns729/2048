88d3655283a99e291cb88407998ea761ff532b1e
Seth Kahn <kahns729@gmail.com>
2014-04-10 21:59:50 -0400

    trying to fix CORS

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ function sortDescending(scoresArray){
 }
 
 
-app.all('*', function(req, res, next) {
+app.all('/*', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
